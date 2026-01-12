5b78805fecdf8bdb25fe27ef7a716ab89b874691
Cortronic <cortronic@googlemail.com>
2026-01-12 10:54:06 +0100

    Initial commit

---
+++ airflow_meter/Readme .txt
@@ -0,0 +1,5 @@
+About
+on going project for my thesis. 
+An esp32 based airflow sensor that utilize the bernoulli principle of pressure to calculate the airflow velocity.
+Subsequently measure the airflow rate.
+Providing real-time data for various applications such as HVAC systems, environmental monitoring, or industrial processes. delta
---
+++ src/main.cpp
@@ -0,0 +1,564 @@
+#include <Arduino.h>
+
+#include <driver/ledc.h>
+#include <driver/dac.h>
+#include <Wire.h>
+#include <SensirionI2CSdp.h>
+// #include <Adafruit_ADS1X15.h>
+// #include <SH1106Wire.h>
+// #include <OLEDDisplayUi.h>
+#include <Adafruit_SH110X.h>
+#include <Fonts/Picopixel.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <PID_v1.h>
+#include <Smoothed.h>
+#include <LittleFS.h>
+
+// Pin definitions
+const int PWM_FAN_PIN = 27;   // 12 = GPIO 32
+const int PWM_CAL_PIN = 13;
+const int CAL_PIN = A0;   // GPIO 36 = A0 = VP
+const int ZERO_PIN = A6;  // GPIO 34 = A6
+const int FLOW_PIN = A7;  // GPIO 35 = A7, uses any valid Ax pin as you wish
+
+const int I2C_SDA0_PIN = 21;
+const int I2C_SCL0_PIN = 22;
+const int I2C_SDA1_PIN = 25;
+const int I2C_SCL1_PIN = 26;
+
+// Display
+const int I2C_ADDRESS_DISPLAY = 0x3C;
+const int I2C_ADDRESS_BME280  = 0x76;
+const int I2C_ADDRESS_SDP810  = 0x25;
+
+// PWM settings
+const int PWM_FAN_CHAN = LEDC_CHANNEL_0;
+const int PWM_CAL_CHAN = LEDC_CHANNEL_4;
+const int PWM_FREQ = 25000;    // 25 kHz frequency for computer fans
+const int CAL_FREQ = 5000;     // 5 KHz
+const int PWM_RESOLUTION = 10; // 10-bit resolution (0-1023)
+const int CAL_RESOLUTION = 12; // 12-bit resolution (0-4095)
+
+// Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+// Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+Smoothed<float> zeroPressure;
+Smoothed<float> flowPressure;
+Smoothed<float> adcCalibration;
+Smoothed<float> pressureAmbient;
+Smoothed<float> temperatureAmbient;
+Smoothed<float> humidityAmbient;
+
+// Twee hardware I2C bussen
+TwoWire I2C_A = TwoWire(0);
+TwoWire I2C_B = TwoWire(1);
+
+// Initialize the OLED display using Wire library
+// ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically
+// based on your board's pins_arduino.h 
+// e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+// SH1106Wire      display(I2C_ADDRESS_DISPLAY, I2C_SDA0_PIN, I2C_SCL0_PIN, GEOMETRY_128_64);
+Adafruit_SH1106G display(128, 64, &I2C_A);
+Adafruit_BME280  bme280;     // I2C
+
+// lookup tabel
+int lookupTable[4096];
+int calibrateTable[4096];
+
+// Define Variables we'll be connecting to
+double Setpoint = 2230, Input, Output;
+
+// Specify the links and initial tuning parameters
+double Kp=0.2, Ki=0.5, Kd=0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, REVERSE);
+
+float offsetZero;
+float offsetFlow;
+
+static void  initDisplay(void);
+static void  displayMeasurements();
+static void  initBME280();
+static float calculateFlow(float dP);
+static float calculateFlowCompensated(float dP);
+static void  drawString(int16_t x, int16_t y, const String &text);
+void         calibrateAdc();
+const char*  readLine(File file);
+int          readFile(const char* filename);
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("\nAirflowmeter is starting up...");
+
+  // Configure PWM
+  Serial.println("Setup PWM");
+  ledcSetup(PWM_FAN_CHAN, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttachPin(PWM_FAN_PIN, PWM_FAN_CHAN);
+  ledcSetup(PWM_CAL_CHAN, CAL_FREQ, CAL_RESOLUTION);
+  ledcAttachPin(PWM_CAL_PIN, PWM_CAL_CHAN);
+ 
+  // Set initial fan speed to zero
+  ledcWrite(PWM_FAN_CHAN, 0);
+  ledcWrite(PWM_CAL_CHAN, 0);
+  delay(1000);
+
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+/*
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }
+
+  // Start the first conversion.
+  ads.startADCReading(ADS1X15_REG_CONFIG_MUX_DIFF_0_1, false); //continuous=false
+*/
+
+  analogReadResolution(12);
+
+  // Start both busses
+  I2C_A.begin(I2C_SDA0_PIN, I2C_SCL0_PIN, 700000);   // SDA, SCL
+  I2C_B.begin(I2C_SDA1_PIN, I2C_SCL1_PIN, 100000);
+
+  Serial.println("Setup Display");
+  initDisplay();
+  delay(1000);
+  Serial.println("Setup BME280");
+  initBME280();
+  delay(1000);
+
+
+  Serial.println("setup smoothed average.");
+  zeroPressure.begin(SMOOTHED_AVERAGE, 50);
+  flowPressure.begin(SMOOTHED_AVERAGE, 100);
+  adcCalibration.begin(SMOOTHED_AVERAGE, 100);
+  pressureAmbient.begin(SMOOTHED_AVERAGE, 40);
+  humidityAmbient.begin(SMOOTHED_AVERAGE, 40);
+  temperatureAmbient.begin(SMOOTHED_AVERAGE, 40);
+  delay(1000);
+
+  Serial.println("setup LitteFS.");
+  if (!LittleFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    delay(1000);
+    Serial.println("starting calibration procedure.");
+    calibrateAdc();
+  } else {
+    Serial.printf("LittleFS: totalbytes: %u, usedbytes: %u.\n", LittleFS.totalBytes(), LittleFS.usedBytes());
+    Serial.println("Reading file: /luptable.txt");
+    if (readFile("/luptable.txt") < 0) {
+      Serial.println("Reading file failed");
+      delay(1000);
+      Serial.println("starting calibration procedure.");
+      calibrateAdc();
+    }
+  }
+
+  //
+  Serial.println("Getting setpoint zeropressure control."); 
+  for (size_t i = 0; i < 100; i++) {
+    zeroPressure.add(lookupTable[analogRead(ZERO_PIN)]);
+    delay(2);
+  }
+  // initialize the variables we're linked to
+  Setpoint = Input = offsetZero = zeroPressure.get();
+
+  // turn the PID on
+  Serial.println("turn the PID on.");
+  myPID.SetMode(AUTOMATIC);
+
+  //
+  Serial.println("Getting offset flowsensor."); 
+  for (size_t i = 0; i < 100; i++) {
+    flowPressure.add(lookupTable[analogRead(FLOW_PIN)]);
+    delay(2);
+  }
+  offsetFlow = flowPressure.get();
+  Serial.print("Setup done...\n\n");
+  delay(2000);
+}
+
+void loop() {
+  static uint32_t loopcnt = 0;
+  static uint32_t last_millis = 0;
+
+  flowPressure.add(lookupTable[analogRead(FLOW_PIN)]);
+  zeroPressure.add(lookupTable[analogRead(ZERO_PIN)]);
+
+  if (millis() >= last_millis + 100) {
+    last_millis = millis();
+
+    Input = zeroPressure.get();
+    myPID.Compute();
+    // Convert Output(0..255) to PWM duty cycle (0-1023 for 10-bit resolution)
+    int fanSpeed = Output * 4.0;
+    // Apply PWM to fan
+    ledcWrite(PWM_FAN_CHAN, fanSpeed);
+    
+    // every second
+    if (loopcnt % 10 == 0) {
+
+      // Only needed in forced mode! In normal mode, you can remove the next line.
+      bme280.takeForcedMeasurement(); // has no effect in normal mode
+
+      // get the measurements from the BME280
+      pressureAmbient.add(bme280.readPressure() / 100.0); // convert from Pa to hPa
+      temperatureAmbient.add(bme280.readTemperature());
+      humidityAmbient.add(bme280.readHumidity());
+
+      Serial.printf("Zero presssure: %f (%fV)\n", Input, 3.3 * Input/4096);
+      Serial.printf("PWM fan dutycycle: %.1f%%\n", Output/255.0 * 100.0);
+    }
+    
+    // every 2 seconds
+    if (loopcnt++ % 20 == 0) {
+      displayMeasurements();
+    }
+  } else {
+    delay(2);
+  }
+}
+
+void initBME280(void) {
+  
+  if (bme280.begin(I2C_ADDRESS_BME280, &I2C_A)) {
+
+    //weather monitoring scenario
+    bme280.setSampling(Adafruit_BME280::MODE_FORCED,
+                    Adafruit_BME280::SAMPLING_X1, // temperature
+                    Adafruit_BME280::SAMPLING_X1, // pressure
+                    Adafruit_BME280::SAMPLING_X1, // humidity
+                    Adafruit_BME280::FILTER_OFF);
+                    
+    drawString(0, 12, "Setup BME280 ");                
+    delay(1000);
+
+  } else {
+    char message[32];
+    drawString(5, 10, "No valid BME280");
+    delay(5000);
+    snprintf(message, sizeof(message), "SensorID: 0x%02X", bme280.sensorID());
+    drawString(5, 10, message);
+    delay(5000);
+  }
+}
+//////////////////////////////////////////////////////////////////////////
+
+static void setOledContrast(uint8_t contrast, uint8_t precharge, uint8_t comdetect) {
+  display.oled_command(SH110X_SETPRECHARGE); //0xD9
+  display.oled_command(precharge); //0xF1 default, to lower the contrast, put 1-1F
+  display.oled_command(SH110X_SETCONTRAST);
+  display.oled_command(contrast);  // 0-255
+  display.oled_command(SH110X_SETVCOMDETECT); //0xDB, (additionally needed to lower the contrast)
+  display.oled_command(comdetect); //0x40 default, to lower the contrast, put 0
+  display.oled_command(SH110X_DISPLAYALLON_RESUME);
+  display.oled_command(SH110X_NORMALDISPLAY);
+  display.oled_command(SH110X_DISPLAYON);
+}
+//////////////////////////////////////////////////////////////////////////
+
+static void setOledBrightness(uint8_t brightness) {
+  uint8_t contrast = brightness;
+  if (brightness < 128) {
+    // Magic values to get a smooth/ step-free transition
+    contrast = brightness * 1.171;
+  } else {
+    contrast = brightness * 1.171 - 43;
+  }
+
+  uint8_t precharge = 241;
+  if (brightness == 0) {
+    precharge = 0;
+  }
+  uint8_t comdetect = brightness / 8;
+
+  setOledContrast(contrast, precharge, comdetect);
+}
+//////////////////////////////////////////////////////////////////////////
+
+static void initDisplay(void) {
+  display.begin(0x3C, true);
+  //display.setBrightness(0x60);
+  setOledBrightness(0x60);
+  //display.flipScreenVertically(); // turn the display upside down;
+  // ArialMT_Plain_10, ArialMT_Plain_16, ArialMT_Plain_24
+  //display.setFont(ArialMT_Plain_16);
+  //display.setTextSize(16);
+  //display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.clearDisplay();
+  display.setTextColor(SH110X_WHITE);
+  display.setTextSize(1);          // schaalfactor
+  //display.drawString(0, 24, "Display ready");
+  display.setCursor(0,24);
+  display.print("Display ready");
+  display.display();
+  delay(1000);
+}
+//////////////////////////////////////////////////////////////////////////
+
+static void printAlignCenter(const char *text, int16_t x, int16_t y) {
+  int16_t x1, y1;
+  uint16_t w, h;
+
+  display.getTextBounds(text, 0, y, &x1, &y1, &w, &h);
+  display.setCursor(x-(w>>1), y);
+  display.print(text);
+}
+//////////////////////////////////////////////////////////////////////////
+
+static void printAlignRight(const char *text, int16_t x, int16_t y) {
+  int16_t x1, y1;
+  uint16_t w, h;
+
+  display.getTextBounds(text, 0, y, &x1, &y1, &w, &h);
+  display.setCursor(x-w, y);
+  display.print(text);
+}
+//////////////////////////////////////////////////////////////////////////
+
+// Draws a string at the given location
+static void drawString(int16_t x, int16_t y, const String &text) {
+  display.clearDisplay();
+  // display.setTextAlignment(TEXT_ALIGN_LEFT);
+  // display.setFont(ArialMT_Plain_10);
+  display.setTextSize(1);
+  // display.drawString(x, y, text);
+  display.setCursor(x, y);
+  display.print(text);
+  display.display();
+}
+//////////////////////////////////////////////////////////////////////////
+
+static void displayMeasurements() {
+  char message[32];
+  float pz = zeroPressure.get() - offsetZero;
+  float pf = flowPressure.get() - offsetFlow;
+  float flow = calculateFlowCompensated(pf);
+  float temperature = temperatureAmbient.get();
+  float humidity = humidityAmbient.get();
+  float pressure = pressureAmbient.get();
+  
+  display.clearDisplay();
+
+  // display P_flow and P_zero
+  // snprintf(message, sizeof(message),"Pf %.1f Pa", pf);
+  // display.setFont(ArialMT_Plain_10)
+  // display.setTextAlignment(TEXT_ALIGN_LEFT);
+  // display.drawString(8, 0, message);
+  // display.setFont(&TomThumb);
+  // display.setTextColor(SH110X_WHITE);
+  // display.setFont(&Picopixel);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.printf("f %.1fPa", pf);
+  snprintf(message, sizeof(message),"z %.1fPa", pz);
+  // display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  // display.drawString(119, 0, message);
+  printAlignRight(message, 127,0);
+  
+  // display Flow
+  snprintf(message, sizeof(message),"%.1f", flow);
+  // display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(); // standaard font
+  display.setTextSize(2);
+  // display.drawString(64, 14, message);
+  printAlignCenter(message, 63,18);
+  // display.setFont(ArialMT_Plain_10);
+  display.setTextSize(1);
+  // display.setTextAlignment(TEXT_ALIGN_LEFT);
+  // display.drawString(8, 21, "FLow");
+  display.setCursor(8, 21);
+  display.print("FLow");
+  // display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  // display.drawString(119,21, "m³/h");
+  printAlignRight("m3/h", 119, 21);
+
+  // display temperature
+  // display.setFont(ArialMT_Plain_10);
+  display.setTextSize(1);
+  // display.setTextAlignment(TEXT_ALIGN_LEFT);
+  snprintf(message, sizeof(message),"%.1f C", temperature);
+  // display.drawString(8, 41, message);
+  display.setCursor(8, 41);
+  display.print(message);
+
+  // display humidity
+  // display.setFont(ArialMT_Plain_10);
+  display.setTextSize(1);
+  // display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  snprintf(message, sizeof(message),"%.1f %%RH", humidity);
+  // display.drawString(119, 41, message);
+  printAlignRight(message, 119, 41);
+
+  // display pressure
+  // display.setFont(ArialMT_Plain_10);
+  display.setTextSize(1);
+  // display.setTextAlignment(TEXT_ALIGN_CENTER);
+  snprintf(message, sizeof(message),"%.1f hPa", pressure);
+  // display.drawString(64, 54, message);
+  printAlignCenter(message, 63, 56);
+ 
+  display.display();
+}
+//////////////////////////////////////////////////////////////////////////
+
+/**
+  *                    ____________
+  *                  /  dP * T
+  * Q  = 0.0723  \  / ____________  (m³/s)
+  *               \/      Pabs
+  *
+  *                                  ____________ 
+  *                                 /  dP * T  
+  * Q = 3600 x 0.0723 = 260.28  \  / ____________  (m³/h)
+  *                             \/      Pabs
+  * 
+*/
+
+static float calculateFlowCompensated(float dP) {
+  float Pabs = pressureAmbient.get() * 100.0;
+  float Tamb = temperatureAmbient.get() + 273.15;
+
+  return dP > 0.0 ? 260.28 * sqrt(dP * Tamb / Pabs) : 0.0; // (m³/h)
+}
+//////////////////////////////////////////////////////////////////////////
+
+static float calculateFlow(float dP) {
+
+  //float v1 = sqrt(dP/1.509); // (m/s)
+  //float v1 = 0.814 * sqrt(dP); // (m/s)
+  //float Q = 0.00478 * v1; // (m³/s)
+  //return Q * 3600.0; // (m³/h)
+  //return 17.208 * v1; // (m³/h)
+  return dP > 0.0 ? 14.00732 * sqrt(dP) : 0.0; // (m³/h)
+}
+//////////////////////////////////////////////////////////////////////////
+
+// generate LUT - reverse array
+void createLUT() {
+  int min_idx = 0;
+  int last_pos = 0;
+  int diff = 1;
+  int last_val = calibrateTable[0];
+
+  Serial.println("Reversing data / Generating LUT...");
+
+  for (size_t i = 0; i < sizeof(lookupTable) / sizeof(int); i++) {
+    if (--diff <= 0) {
+      while (calibrateTable[last_pos] <= last_val) {
+        last_pos++;
+      }
+      diff = calibrateTable[last_pos] - last_val;
+      last_val = calibrateTable[last_pos];
+    }
+    lookupTable[i] = min_idx;
+    min_idx = last_pos;
+  }
+}
+
+void calibrateAdc() {
+  
+  delay(1000);
+  for (size_t i = 0; i < sizeof(calibrateTable) / sizeof(int); i++) {
+    ledcWrite(PWM_CAL_CHAN, i);
+    // Serial.printf("iteration i: %u\n", i);
+    delay(10);
+    for (size_t j = 0; j < 100; j++) {
+      delayMicroseconds(200);
+      adcCalibration.add(analogRead(CAL_PIN));
+    }
+    calibrateTable[i] = adcCalibration.get() + 0.5;
+    Serial.printf("Cal[%u]: %d (%0.3fV)\n", i, calibrateTable[i], 3.3 * calibrateTable[i] / 4096);
+    delay(10);
+  }
+
+  // 3. check if values are ascending and fix if not
+  Serial.println("Checking if ADC values are ascending and fixing them if not...");
+  for (size_t i = 1; i <  sizeof(calibrateTable) / sizeof(int); i++) {
+    if (calibrateTable[i] < calibrateTable[i - 1]) {
+      Serial.printf("Warning: values not ascending, index: %u\n", i);
+      calibrateTable[i] = calibrateTable[i-1];
+    }
+  }
+
+  // 5. generate LUT - reverse array
+  createLUT();
+
+  // print the look-up table
+  Serial.print("Priting the ADC correction look-up table, to be used in your code.\n\n");
+  Serial.print("const float ADC_LUT[4096] = {");
+  for (size_t i = 0; i < sizeof(lookupTable) / sizeof(int); i++) {
+    if (i % 16 == 0)
+      Serial.println();
+    Serial.printf("%d,", lookupTable[i]);
+  }
+  Serial.print("\n};\n\n");
+}
+
+int readFile(const char* filename) {
+  const char *buf, *p;
+  size_t i = 0, j = 0;
+
+  File file = LittleFS.open(filename, "r");
+  if (!file) {
+    Serial.printf("Failed to open file %s for reading\n", filename);
+    return -1;
+  }
+  
+  Serial.println("File Content:");
+  delay(1000
+  );
+
+  while ((buf = readLine(file)) != nullptr) {
+    p = buf;
+    Serial.println(buf);
+    while (sscanf(p, "%d,", &lookupTable[i]) == 1) {
+      i++;
+      p = strchr(p, ',');
+      if (i >= 4096 || p == nullptr)
+        break;
+      p++;
+    }
+    do {
+      Serial.printf("%d,", lookupTable[j++]);
+    } while ((j % 16) != 0);
+    Serial.println("");
+    if (i >= 4096)
+      break;
+    // memset((void*)buf, 0, 128);
+  }
+  file.close();
+  return 1; 
+}
+
+const char* readLine(File file) {
+  static char buf[128];
+  size_t i = 0;
+  char c;
+  while (file.available()) {
+    if ((c = file.read()) != '\n') {
+       buf[i++] = c;
+       if (i >= sizeof(buf)) {
+          // bufferoverlow
+          return nullptr;
+       }
+    } else {
+       break;
+    }
+  }
+  if (i) {
+    buf[i] = '\0';
+    return buf;
+  }
+  return nullptr;
+}
